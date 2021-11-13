a1f0698a612deb543324ea68a63f764e1e4651a6
Con <50535379+ConnorTippets@users.noreply.github.com>
2021-11-13 17:37:56 -0600

    add box2d code

--- scripts/RenderPhysics.js
+++ scripts/RenderPhysics.js
@@ -10,5 +10,15 @@ function main() {
     gl.clearColor(1.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
     
+    const Box2D = Box2D.Box2D;
+    Box2D().then(function(Box2D) {
+        var gravity = new Box2D.b2Vec2(0.0, -10.0);
+        var world = new Box2D.b2World(gravity);
+        world.SetAllowSleeping(false);
+        var bd = new Box2D.b2BodyDef();
+        var ground = world.CreateBody(bd);
+        var a = 0.5;
+        var shape = new Box2D.b2PolygonShape();
+        shape.SetAsBox(a, a);
 }
 window.onload = main;
