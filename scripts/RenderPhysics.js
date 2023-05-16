c9505fc8e36c6950b1a5ef50845619403e6b4e45
Con <50535379+ConnorTippets@users.noreply.github.com>
2023-05-15 21:44:36 -0500

    imagine
    IMAGEIGNKEAKN

--- scripts/RenderPhysics.js
+++ scripts/RenderPhysics.js
@@ -1,4 +1,5 @@
-import Box2D from "./scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/Box2D.min.js";
+//if this works i will actually-
+import Box2D from "./scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/Box2D.min.js";
 function main() {
     const canvas = document.querySelector("#glCanvas");
     const gl = canvas.getContext("webgl");
